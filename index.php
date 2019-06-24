1a921ac2ec92d49385c6a93f39e6442b05116754
irenedebo <irendebo@gmail.com>
2019-06-24 21:47:23 +0700

    Connecting to database

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = dicodingirenserver.database.windows.net;
+    $user = irenedeboraa;
+    $pass = Panjaitan17;
+    $db = dicodingdb;
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
